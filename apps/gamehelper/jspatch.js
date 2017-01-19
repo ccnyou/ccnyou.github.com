c6be4618ad182f8ab4484cc64270768b5928641d
ccnyou <ccnyou@qq.com>
2017-01-19 17:48:02 +0800

    * patch update

--- apps/gamehelper/jspatch.js
+++ apps/gamehelper/jspatch.js
@@ -1,10 +1,10 @@
 defineClass('ChatMessageViewController', {
     _getIconHeight_width: function(linksDictionary, width) {
-
+        var iconHeight = 0;
         var imageHeight = linksDictionary.yoyo_integerForKey("h");
         var imageWeight = linksDictionary.yoyo_integerForKey("w");
         if (imageWeight) {
-            var iconHeight = imageHeight / imageWeight * width;
+            iconHeight = imageHeight / imageWeight * width;
         }
         return iconHeight;
     },
