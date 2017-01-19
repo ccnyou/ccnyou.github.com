49b9fac3cf3fdd3f1fe44427b9326699cb6fb652
ccnyou <ccnyou@qq.com>
2017-01-19 17:44:42 +0800

    * 尝试通过jspatch修复一个本地问题

--- apps/gamehelper/jspatch.js
+++ apps/gamehelper/jspatch.js
@@ -1,1 +1,11 @@
-helloworld
+defineClass('ChatMessageViewController', {
+    _getIconHeight_width: function(linksDictionary, width) {
+
+        var imageHeight = linksDictionary.yoyo_integerForKey("h");
+        var imageWeight = linksDictionary.yoyo_integerForKey("w");
+        if (imageWeight) {
+            var iconHeight = imageHeight / imageWeight * width;
+        }
+        return iconHeight;
+    },
+});
